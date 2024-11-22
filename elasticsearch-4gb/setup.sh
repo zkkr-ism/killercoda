96dc9274cf5146404eb748f4ce65d4849eedb55b
Ahmad Muzakkir Bin Ismail <50354321+zkkr-ism@users.noreply.github.com>
2024-11-23 01:12:34 +0800

    update setup.sh

--- elasticsearch-4gb/setup.sh
+++ elasticsearch-4gb/setup.sh
@@ -3,18 +3,18 @@
 cat <<EOF > docker-compose.yml
 version: '3'
 services:
-elasticsearch:
-  image: docker.elastic.co/elasticsearch/elasticsearch:7.13.2
-  environment:
-    - discovery.type=single-node
-  ports:
-    - 9200:9200
-kibana:
-  image: docker.elastic.co/kibana/kibana:7.13.2
-  ports:
-    - 5601:5601
-  depends_on:
-    - elasticsearch
+ elasticsearch:
+   image: docker.elastic.co/elasticsearch/elasticsearch:7.13.2
+   environment:
+     - discovery.type=single-node
+   ports:
+     - 9200:9200
+ kibana:
+   image: docker.elastic.co/kibana/kibana:7.13.2
+   ports:
+     - 5601:5601
+   depends_on:
+     - elasticsearch
 EOF
 
 # Deploy the services using Docker Compose
